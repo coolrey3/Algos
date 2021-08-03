e7d8b4f8b89989707187435509fa6ca731da0ad0
Ray <coolrey3@gmail.com>
2021-08-03 16:52:59 -0400

    added descending

--- sort/bubble/bubble_sort.py
+++ sort/bubble/bubble_sort.py
@@ -19,5 +19,22 @@ class BubbleSort():
         print('Array sorted: ',self.numberlist)
         return self.numberlist
 
+    def sortDescending(self):
+        solved = sorted(self.numberlist,reverse=True)
+        print(solved)
+        sortComplete = False
+        while sortComplete == False:
+            for x in range(1,len(self.numberlist)):
+                if self.numberlist[x] < self.numberlist[x+1]:
+                    self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
+            
+            if self.numberlist == solved:
+                sortComplete = True
+           
+
+        print('Array sorted: ',self.numberlist)
+        return self.numberlist
+
+
 bs = BubbleSort([6,4,3,7,4,3,2,7,4,3,6,7,8,4,2,0])
-bs.sortAscending()
+bs.sortDescending()
