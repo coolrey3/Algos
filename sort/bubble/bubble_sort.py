02dca66ae0cf2df7eb70e914f6e9fba877d921ee
Ray <coolrey3@gmail.com>
2021-08-03 16:45:22 -0400

    bubble sort ascending

--- sort/bubble/bubble_sort.py
+++ sort/bubble/bubble_sort.py
@@ -3,22 +3,25 @@ class BubbleSort():
         print(numberlist)
         print('--------------')
         self.numberlist = numberlist
-        sorted = False
-        self.sorted = sorted
+        self.solved = sorted(self.numberlist)
 
-    def sort(self):
-        while self.sorted == False:
+    def sortAscending(self):
+        sorted = False
+        while sorted == False:
             for x in range(len(self.numberlist)-1):
                 # print(x)
                 if self.numberlist[x] > self.numberlist[x+1]:
                     self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
             
-            if self.numberlist == self.numberlist.sort():
-                self.sorted = True
+            if self.numberlist == self.solved:
+                sorted = True
+            # else:
+            #     print('still not sorted')
+                # print(self.numberlist)
             
 
-        print(self.numberlist)
+        print('Array sorted: ',self.numberlist)
         return self.numberlist
 
 bs = BubbleSort([5,3,6,2,1,7,9])
-bs.sort()
+bs.sortAscending()
