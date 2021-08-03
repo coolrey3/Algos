bb3865beb6378512eb9fd2ac7eb1aa816f0cfc42
Ray Mercedes <contact@RayMercedes.com>
2021-08-03 17:47:14 -0400

    added swap method

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sort_algorithms.bubble_sort import * 
 from sort_algorithms.insertion_sort import * 
 
-bs = BubbleSort([1,5,9])
+bs = BubbleSort([5,8,4,6,4,8,9,4,7,1,2,3,4,5,8,7,1,10,3,6,4,6,8,5,6,4,6])
 bs.sortAscending()
 bs.sortDescending()
 
--- sort_algorithms/bubble_sort.py
+++ sort_algorithms/bubble_sort.py
@@ -10,7 +10,8 @@ class BubbleSort():
         while sortComplete == False:
             for x in range(len(self.numberlist)-1):
                 if self.numberlist[x] > self.numberlist[x+1]:
-                    self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
+                    self.swap(self.numberlist,x,x+1)
+                    # self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
                     isSorted[x] = False
                 else:
                     isSorted[x] = True
@@ -25,7 +26,8 @@ class BubbleSort():
         while sortComplete == False:
             for x in range(0,len(self.numberlist)-1):
                 if self.numberlist[x] < self.numberlist[x+1]:
-                    self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
+                    self.swap(self.numberlist,x,x+1)
+                    # self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
                     isSorted[x] = False
                 else:
                     isSorted[x] = True
@@ -33,4 +35,9 @@ class BubbleSort():
                 sortComplete = True
         print('Array sorted with Bubble Sort-Descending:\n',self.numberlist)
         return self.numberlist
+
+    def swap(self,array,index1,index2):
+        array[index1] , array[index2] = array[index2],array[index1]
+        return
+
 
