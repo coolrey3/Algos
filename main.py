43ce853a6348538b247e6f14d32f97c15766234f
Ray Mercedes <contact@RayMercedes.com>
2021-08-03 17:31:39 -0400

    bubble sort working, from scratch

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sort.insertion_sort import *
 
 bs = BubbleSort([6,4,3,7,4,3,2,7,4,3,6,7,8,4,2,0])
 bs.sortAscending()
+bs.sortDescending()
 
 ins = InsertionSort()
 ins
--- sort/bubble_sort.py
+++ sort/bubble_sort.py
@@ -1,34 +1,39 @@
 class BubbleSort():
     def __init__(self,numberlist):
-        print(numberlist)
+        print('Raw numberlist: ',numberlist)
         print('--------------')
         self.numberlist = numberlist
 
     def sortAscending(self):
-        solved = sorted(self.numberlist)
+        isSorted = {}
         sortComplete = False
         while sortComplete == False:
             for x in range(len(self.numberlist)-1):
                 if self.numberlist[x] > self.numberlist[x+1]:
                     self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
-            if self.numberlist == solved:
+                    isSorted[x] = False
+                else:
+                    isSorted[x] = True
+            if False not in isSorted.values():
                 sortComplete = True
 
         print('Array sorted with Bubble Sort-Ascending:\n',self.numberlist)
         return self.numberlist
 
     def sortDescending(self):
-        solved = sorted(self.numberlist,reverse=True)
-        print(solved)
+        isSorted = {}
+
         sortComplete = False
         while sortComplete == False:
             for x in range(0,len(self.numberlist)-1):
                 if self.numberlist[x] < self.numberlist[x+1]:
                     self.numberlist[x] , self.numberlist[x + 1] = self.numberlist[x + 1],self.numberlist[x]
-            if self.numberlist == solved:
+                    isSorted[x] = False
+                else:
+                    isSorted[x] = True
+
+            if False not in isSorted.values():
                 sortComplete = True
-            # else:
-            #     print(self.numberlist)
 
         print('Array sorted with Bubble Sort-Descending:\n',self.numberlist)
         return self.numberlist
