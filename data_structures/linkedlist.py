04cd939030d3e1a43b7c45927ad92199399565d7
Ray Mercedes <contact@RayMercedes.com>
2021-08-04 17:16:54 -0400

    comments deque

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -1,14 +1,35 @@
 from collections import  deque
 
-ll = deque()
-ll.append('test')
+#Regular Queue - FIFO
+print('Queue')
 
+ll = deque()        #create linked list
+ll.append('test')   #append 'test' to llist
 print(ll)
-ll.append('work')
-
-ll.append('play')
+ll.append('work')   #append 'work' to llist
 print(ll)
-ll.popleft()
+ll.append('play')   #append 'play' to llist
 print(ll)
-
+ll.popleft()        #remove left most value in llist (regular queue)
+print(ll)
+ll.popleft()        #remove left most value in llist (regular queue)
+print(ll)
+ll.popleft()        #remove left most value in llist (regular queue)
+print(ll)
+print()
+print('Stack')
+#Stack - LIFO
+stack = deque()        #create linked list
+stack.append('test')   #append 'test' to llist
+print(stack)
+stack.append('work')   #append 'work' to llist
+print(stack)
+stack.append('play')   #append 'play' to llist
+print(stack)
+stack.pop()            #remove right most value in llist (stack)
+print(stack)
+stack.pop()            #remove right most value in llist (stack)
+print(stack)
+stack.pop()            #remove right most value in llist (stack)
+print(stack)
 
