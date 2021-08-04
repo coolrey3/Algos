32ce8738c388c7d2c0a43c8127f0b374e488e6ba
Ray Mercedes <contact@RayMercedes.com>
2021-08-04 13:39:42 -0400

    linked list comments added

--- data_structures/raw_linked_list.py
+++ data_structures/raw_linked_list.py
@@ -1,23 +1,23 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Node:                 # define Node Class
+    def __init__(self, data): #initialize a new node that takes data parameter
+        self.data = data    # assign data value via parameter to this node
+        self.next = None    #assign pointer to next node(defaults to none)
 
-    def __repr__(self):
-        return self.data
+    def __repr__(self):     #modify string representation when printed 
+        return self.data    #return data value when printing node
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
+class LinkedList:           # define LinkedList Class
+    def __init__(self):     #initialize linked list, no parameters
+        self.head = None    # assign head of linked list to None when initialized
 
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(node.data)
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
+    def __repr__(self):     #modify string representation when printed 
+        node = self.head    #assign head of linked list to node
+        nodes = []          #create empty array for child nodes
+        while node is not None:     #iterate while head node is not None
+            nodes.append(node.data) #append nodes array with node data
+            node = node.next        #head node set to next node (only once)
+        nodes.append("None")        #append None to end of nodes array
+        return " -> ".join(nodes)   #return the list of array with -> between each item
 
 llist = LinkedList()
 print(llist)
