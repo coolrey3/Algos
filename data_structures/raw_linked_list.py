18aaf52d398162c69f9b68eb2b3f40fe75f70e4f
Ray Mercedes <contact@RayMercedes.com>
2021-08-04 14:53:54 -0400

    trie code started

--- data_structures/raw_linked_list.py
+++ data_structures/raw_linked_list.py
@@ -1,4 +1,4 @@
-class Node:                 # define Node Class
+class Node():                 # define Node Class
     def __init__(self, data): #initialize a new node that takes data parameter
         self.data = data    # assign data value via parameter to this node
         self.next = None    #assign pointer to next node(defaults to none)
@@ -6,7 +6,7 @@ class Node:                 # define Node Class
     def __repr__(self):     #modify string representation when printed 
         return self.data    #return data value when printing node
 
-class LinkedList:           # define LinkedList Class
+class LinkedList():           # define LinkedList Class
     def __init__(self):     #initialize linked list, no parameters
         self.head = None    # assign head of linked list to None when initialized
 
@@ -22,6 +22,7 @@ class LinkedList:           # define LinkedList Class
 llist = LinkedList()        #instantiate linkedlist class
 
 first_node = Node('a')      #instantiate first node from Node class
+print(first_node)
 llist.head = first_node     #assign head of linkedlist to first node
 second_node = Node("b")     #instantiate second node from Node class
 third_node = Node("c")      #instantiate third node from Node class
--- data_structures/raw_trie.py
+++ data_structures/raw_trie.py
@@ -1,21 +1,36 @@
-class createTrie():
+class Node():   #define node class
+    def __init__(self,data):    #initiate with data parameter
+        self.data = data        #set data of node from parameter
+        self.child = None       #set child of node
+        self.word = False
+
+    def __repr__(self):
+        return '{} -> {}'.format(self.data,self.child)
+        # return '{} -> {}'.format(self.data,self.child)
+
+class Trie():
     def __init__(self):
-        x={}
-        x ['root'] =''
-        self.x = x
-        self.x['child'] = []
-        self.x['isWord'] = ''
-        print(self.x)
-        current = 0
+        self.root = None
 
+    def __repr__(self):
+        node = self.root
+        children = None
+        # while self.root is not None:
+        # children.append(str(node.child))
+        #     node = node.child
+        # return str(self.root) + "->".join(children)
+        return str(self.root) 
 
-    def newNode(self,child,isWord):
-        if child not in self.x['child']:
-            self.x['child'].append(child)
-        if isWord == True:
-            self.x['isWord'] = isWord
-        print(self.x)
+node1 = Node('c')
+node2 = Node('a')
+node3 = Node('t')
+node4 = Node('c')
+node5 = Node('h')
+node1.child = node2
+node2.child = node3
+node3.child = node4
+node4.child = node5
 
-ct = createTrie()
-ct.newNode('T', False)
-ct.newNode('V', True)
+trie1 = Trie()
+trie1.root = node1
+print(trie1)
