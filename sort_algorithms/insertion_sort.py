af3670c0eba8bf66bd3e7ef556f95745835e4366
Ray Mercedes <contact@RayMercedes.com>
2021-08-05 03:02:38 -0400

    updates

--- sort_algorithms/insertion_sort.py
+++ sort_algorithms/insertion_sort.py
@@ -15,3 +15,33 @@ class InsertionSort():
             self.numberlist[previous+1] =current
         print(self.numberlist)
 
+    def sort(self):
+        A = self.numberlist
+        # itterate over list from index 1 to end
+        # counter to 0
+        # compare current value with previous, if previous is bigger 
+        # insert to right of sorted portion
+        
+        for x in range(1,len(A)):
+            current = A[x]
+            for j in range(x-1,-1,-1):
+                if A[j] > current:
+                    A[j+1] = A[j]
+                else:
+                    A[j+1] = current
+                    break
+                A[j] = current
+        print(A)
+        return A
+
+array1 = [9,6,4,7,5,2,9,4,2,6,4,1,9,2]
+t=InsertionSort(array1)
+c = t.sort()
+v = sorted(array1)
+print(v)
+if c == v:
+    print('true')
+
+
+
+
