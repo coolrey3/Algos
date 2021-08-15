69c7e55a2515865ce1771eea7fa30978a5398120
Ray Mercedes <contact@RayMercedes.com>
2021-08-15 01:38:18 -0400

    updates

--- data_structures/BST_Practice.py
+++ data_structures/BST_Practice.py
@@ -9,6 +9,16 @@ class BinarySearchTree():
 
     def __init__(self) -> None:
         self.root = None
+
+#     def __repr__(self):
+#         # for x in 
+#         current = self.root
+#         while current != None:
+#             print( f'''{current.data}
+#     / \\
+# {current.left.data}    {current.right.data}
+#             ''')
+#             current = current.left
     
     def insert(self,data):
 
@@ -21,28 +31,37 @@ class BinarySearchTree():
         current = self.root
 
         while True:
+            if node.data == current.data: break
             if node.data < current.data:
                 if current.left == None:
                     current.left = node
                     break
-                else:
-                    current = current.left
+                current = current.left
             else:
                 if current.right == None:
                     current.right = node
                     break
-                else:
-                    current = current.right
+                current = current.right
 
         return self
+
+    def find(self,val):
+        ...
 
 b = BinarySearchTree()
 b.insert(15)
 b.insert(5)
-b.insert(3)
+b.insert(5)
+b.insert(11)
+b.insert(17)
+b.insert(33)
+b.insert(30)
+b.insert(0)
 b.insert(2)
 b.insert(45)
 b.insert(25)
 b.insert(41)
 
 print(b.root.right.data)
+print(b)
+
