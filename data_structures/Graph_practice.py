1d832a6adca0647226569ba78c08d6a1f186be1f
Ray Mercedes <contact@RayMercedes.com>
2021-08-16 03:29:41 -0400

    graph traverse added

--- data_structures/Graph_practice.py
+++ data_structures/Graph_practice.py
@@ -25,18 +25,48 @@ class Graph():
             self.adjacenyList[x].remove(v1)
         self.adjacenyList.pop(v1)
         # print(r)
+
+    def dfsRecursive(self,start):
+        visited = []
+        result = []
+        current = start
+        visited.append(current)
+
+        def dfs(current):
+            cur = self.adjacenyList.get(current)
+            if cur != []:
+                for x in cur:
+                    if x not in visited :
+                        visited.append(x)
+                        print(x)
+                        dfs(x)
+                return x
+
+        dfs(current)
+        
+        return visited
+
         
 
 g =Graph()
-g.addVertex('Tokyo')
-g.addVertex('Alanta')
-g.addVertex('Miami')
-g.addVertex('New York')
-g.addEdge('Miami','Alanta')
-g.addEdge('Miami','New York')
+g.addVertex('A')
+g.addVertex('B')
+g.addVertex('C')
+g.addVertex('D')
+g.addVertex('E')
+g.addVertex('F')
+g.addEdge('A','B')
+g.addEdge('A','C')
+g.addEdge('B','D')
+g.addEdge('E','C')
+g.addEdge('D','E')
+g.addEdge('D','F')
+g.addEdge('E','F')
 print(g.adjacenyList)
-g.removeEdge('Miami', 'New York')
+# g.removeEdge('Miami', 'New York')
 
-g.removeVertex('Miami')
+# g.removeVertex('Miami')
 
 print(g.adjacenyList)
+
+g.dfsRecursive('E')
